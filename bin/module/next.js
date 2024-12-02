ea54527315e05a276fa57b33deb6b99bca0fa552
COSWARS <1850214353@qq.com>
2024-12-03 05:57:27 +0800

    2024/12/03 周二 5:57:27.11

--- bin/module/next.js
+++ bin/module/next.js
@@ -1,12 +1,12 @@
-import { downGit } from "../utils/downGit.js";
-import { nextGits } from "../constants/gitUrl.js";
+import { downGit } from '../utils/downGit.js';
+import { nextGits } from '../constants/gitUrl.js';
 const next = (cmd) => {
-    cmd.command('next')
+    cmd
+        .command('next')
         .argument('<apps...>', '项目名称')
-        .description('生成next项目').action((apps) => {
+        .description('生成next项目')
+        .action((apps) => {
         for (const app of apps) {
-            console.log('app', app);
-            continue;
             downGit('direct:' + nextGits.default, app);
         }
     });
--- src/module/next.ts
+++ src/module/next.ts
@@ -1,17 +1,17 @@
-import { Command } from "commander";
-import chalk from "chalk";
-import { downGit } from "../utils/downGit.js";
-import { nextGits } from "../constants/gitUrl.js";
+import { Command } from 'commander';
+import chalk from 'chalk';
+import { downGit } from '../utils/downGit.js';
+import { nextGits } from '../constants/gitUrl.js';
 const next = (cmd: Command) => {
-	cmd.command('next')
-    .argument('<apps...>','项目名称')
-    .description('生成next项目').action((apps)=>{
-        for (const app of apps) {
-            console.log('app',app);
-            continue
-            downGit('direct:'+nextGits.default,app)
-        }
-	})
+  cmd
+    .command('next')
+    .argument('<apps...>', '项目名称')
+    .description('生成next项目')
+    .action((apps) => {
+      for (const app of apps) {
+        downGit('direct:' + nextGits.default, app);
+      }
+    });
 };
 
 export { next };
